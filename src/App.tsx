8e151a41f3f2ad886c86800083dcfc4765441f27
beinyi <39291190+beinyi@users.noreply.github.com>
2022-12-05 16:49:11 +0700

    homepage

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,18 @@ import checkOrderMap from "@guide-maps/checkOrderMap";
 import Request from "@components/Main/Sliders/Request";
 import { navButtons } from "@components/Main/buttons";
 
-// export let nameurl = "/infohelpmobile"; //для боя
-export let nameurl = ""; //для тестирования у себя
 
 function App() {
   return (
     <div className="screen">
       <Routes>
-        <Route path={nameurl+"/"} element={<Menu />} />
-        <Route path={nameurl+"/make_order"} element={<Guide guideMap={servicesMap} credits={servicesCredits} />} />
-        <Route path={nameurl+"/pay_guide"} element={<Guide guideMap={payMap} credits={payCredits} />} />
-        <Route path={nameurl+"/transaction_guide"} element={<Guide guideMap={transactionMap} />} />
-        <Route path={nameurl+"/accruals_guide"} element={<Guide guideMap={accrualsMap} />} />
-        <Route path={nameurl+"/call_to_mc"} element={<Guide guideMap={callMap} credits={callCredits} />} />
-        <Route path={nameurl+"/check_order"} element={<Guide guideMap={checkOrderMap} />} />
+        <Route path={"/"} element={<Menu />} />
+        <Route path={"/make_order"} element={<Guide guideMap={servicesMap} credits={servicesCredits} />} />
+        <Route path={"/pay_guide"} element={<Guide guideMap={payMap} credits={payCredits} />} />
+        <Route path={"/transaction_guide"} element={<Guide guideMap={transactionMap} />} />
+        <Route path={"/accruals_guide"} element={<Guide guideMap={accrualsMap} />} />
+        <Route path={"/call_to_mc"} element={<Guide guideMap={callMap} credits={callCredits} />} />
+        <Route path={"/check_order"} element={<Guide guideMap={checkOrderMap} />} />
 
         <Route path="/dev" element={<Request activation={navButtons[0]} onNextStep={() => {}} />} />
 
